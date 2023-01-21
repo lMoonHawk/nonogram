913e06710ffb84e619a86729071a17adc4753764
lMoonHawk <lmoonhawk@gmail.com>
2023-01-21 03:26:46 +0100

    add functions and optimisations

--- solver_tkinter.py
+++ solver_tkinter.py
@@ -23,7 +23,6 @@ click_direction = None
 
 
 # TODO:
-# Clean redundant code with functions
 # Add space for clues on the GUI
 # Add buttons for solving by steps and in one go
 # Add option for entering clues
@@ -31,32 +30,61 @@ click_direction = None
 # Add option to create nonogram from image
 
 
-def left_click(event):
-    global override, click_coord, click_direction
+def mark(cell, click_type, held=False) -> bool:
+    """Mark the cell being clicked on, or hover over.
 
-    cell = puzzle.find_closest(event.x, event.y)
+    Args:
+        cell (_type_): tag of the cell clicked or hovered over
+        click_type (int): 1 indicates a left click, -1 indicates a right click
+        held (bool, optional): Indicates if the event has been triggered while holding the mouse button down. Defaults to False.
 
-    # Only fill if the item is a cell (+2 for border)
+    Returns:
+        bool: True if a cell cell has been marked
+    """
+    global override
+
+    # Only fill if the item is a small cell (+2 for border)
     x0, _, x1, _ = puzzle.bbox(cell)
-    if x1 - x0 == cell_size + 2:
-        row, col = divmod(cell[0], ncol)
-        state = grid[row][col]
+    if x1 - x0 != cell_size + 2:
+        return False
 
-        if state in [0, -1]:
-            puzzle.itemconfig(cell, fill="black", stipple="")
-            grid[row][col] = 1
+    # Get row, column and current state of the cell in the grid
+    row, col = divmod(cell[0], ncol)
+    state = grid[row][col]
 
-        elif state == 1:
-            puzzle.itemconfig(cell, fill="white", stipple="")
-            grid[row][col] = 0
-
-        # Only modify same type cells if button held
+    # If button is held, state of the cell hovered must be the same as override
+    condition = True
+    if held:
+        condition = override == state
+    # If button is pressed, override becomes the current state
+    else:
         override = state
-        click_coord = event.x, event.y
-        click_direction = None
+
+    # Click is of the same time as the cell: erase
+    if condition and state == click_type:
+        puzzle.itemconfig(cell, fill="white", stipple="")
+        grid[row][col] = 0
+    # Left click a different cell type
+    if click_type == 1 and condition and state in [0, -1]:
+        puzzle.itemconfig(cell, fill="black", stipple="")
+        grid[row][col] = 1
+    # Right click a different cell type
+    if click_type == -1 and condition and state in [1, 0]:
+        puzzle.itemconfig(cell, stipple="gray50", fill="#AAAAAA")
+        grid[row][col] = -1
+
+    return True
 
 
-def held_left_click(event):
+def snap_line(event: tk.Event) -> tuple:
+    """Keeps track of the direction of the line the user went for.
+
+    Args:
+        event (tk.Event): event triggered on mouse button held down
+
+    Returns:
+        tuple: Coordinates of the mouse, projected to the line created from the initially clicked cell
+    """
     global overrides, click_coord, click_direction
     click_x, click_y = click_coord
 
@@ -75,82 +103,41 @@ def held_left_click(event):
         x = click_x
         y = event.y
 
-    cell = puzzle.find_closest(x, y)
-
-    # Only fill if the item is a cell (+2 for border)
-    x0, _, x1, _ = puzzle.bbox(cell)
-    if x1 - x0 == cell_size + 2:
-        row, col = divmod(cell[0], ncol)
-        state = grid[row][col]
-
-        if override == state and state in [0, -1]:
-            puzzle.itemconfig(cell, fill="black", stipple="")
-            grid[row][col] = 1
-
-        elif override == state and state == 1:
-            puzzle.itemconfig(cell, fill="white", stipple="")
-            grid[row][col] = 0
+    return x, y
 
 
-def right_click(event):
+def left_click(event: tk.Event):
     global override, click_coord, click_direction
 
     cell = puzzle.find_closest(event.x, event.y)
 
-    # Only fill if the item is a cell (+2 for border)
-    x0, y0, x1, _ = puzzle.bbox(cell)
-    if x1 - x0 == cell_size + 2:
-        row, col = divmod(cell[0], ncol)
-        state = grid[row][col]
-
-        if state in [1, 0]:
-            puzzle.itemconfig(cell, stipple="gray50", fill="#AAAAAA")
-            grid[row][col] = -1
-
-        elif state == -1:
-            puzzle.itemconfig(cell, fill="white", stipple="")
-            grid[row][col] = 0
-
-        # Only modify same type cells if button held
-        override = state
+    if mark(cell, 1):
         click_coord = event.x, event.y
         click_direction = None
 
 
-def held_right_click(event):
-    global overrides, click_coord, click_direction
-    click_x, click_y = click_coord
-
-    if click_direction is None:
-        # User went for a horizontal line
-        if abs(event.x - click_x) > abs(event.y - click_y):
-            click_direction = 0
-        # User went for a vertical line
-        else:
-            click_direction = 1
-
-    if click_direction == 0:
-        x = event.x
-        y = click_y
-    else:
-        x = click_x
-        y = event.y
-
+def held_left_click(event: tk.Event):
+    x, y = snap_line(event)
     cell = puzzle.find_closest(x, y)
 
-    # Only fill if the item is a cell (+2 for border)
-    x0, y0, x1, _ = puzzle.bbox(cell)
-    if x1 - x0 == cell_size + 2:
-        row, col = divmod(cell[0], ncol)
-        state = grid[row][col]
+    mark(cell, 1, held=True)
 
-        if override == state and state in [1, 0]:
-            puzzle.itemconfig(cell, stipple="gray50", fill="#AAAAAA")
-            grid[row][col] = -1
 
-        elif override == state and state == -1:
-            puzzle.itemconfig(cell, fill="white", stipple="")
-            grid[row][col] = 0
+def right_click(event: tk.Event):
+    global override, click_coord, click_direction
+
+    cell = puzzle.find_closest(event.x, event.y)
+
+    if mark(cell, -1):
+        click_coord = event.x, event.y
+        click_direction = None
+
+
+def held_right_click(event: tk.Event):
+    x, y = snap_line(event)
+    cell = puzzle.find_closest(x, y)
+
+    mark(cell, -1, held=True)
 
 
 # Create the puzzle
@@ -174,7 +161,6 @@ for i in range(nrow // 5):
         row.append(big_rect)
     blocks.append(row)
 
-# puzzle.bind("<Motion>", detect_override)
 puzzle.bind("<Button-1>", left_click)
 puzzle.bind("<B1-Motion>", held_left_click)
 puzzle.bind("<Button-3>", right_click)
